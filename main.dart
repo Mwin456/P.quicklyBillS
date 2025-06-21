d053c0dc7463e031b8dc9a1f2a196aa03df4ed2b
Mwin456 <mwinplay03@gmail.com>
2025-06-21 21:40:11 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,21 @@
-// Dummy content for main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PquicklyBillApp());
+}
+
+class PquicklyBillApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'P.quicklyBill',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("P.quicklyBill")),
+        body: Center(child: Text("Recharge App Working!")),
+      ),
+    );
+  }
+}
